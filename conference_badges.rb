48d2e654cb77e3397f7c87f000d42e7bf26831c7
Mac Dowell Prosper <macdowell@noukod.com>
2018-09-18 04:50:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,7 @@ def printer(names)
   batch_badge_creator(names).each do |badge|
     puts badge
   end
-  assign_rooms names.each do |speaker|
+  assign_rooms(names).each do |speaker|
     puts speaker
   end
 end
