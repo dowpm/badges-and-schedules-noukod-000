1a63ff9be1ac5a0216bfa659992f69bdd97d0779
Mac Dowell Prosper <macdowell@noukod.com>
2018-09-18 04:47:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,9 +10,8 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(speakers)
-  arr = Array.new
-  speakers.each_with_index do |val, index|
-    arr.push( "Hello, #{val}! You'll be assigned to room #{index+1}!")
+  speakers.each_with_index.map do |val, index|
+    "Hello, #{val}! You'll be assigned to room #{index+1}!"
   end
   arr
 end
